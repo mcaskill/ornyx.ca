f21d232c3843a3a533e3f462ce2912b43798583f
Chauncey McAskill <chauncey@mcaskill.ca>
2022-10-02 11:36:21 -0400

    Clean-up index
    Changed:
    - Use short array syntax.
    
    Removed:
    - Unused `ROOT` constant.

--- index.php
+++ index.php
@@ -1,44 +1,40 @@
 <?php
 
-if (strstr($_SERVER['HTTP_HOST'], 'ornyx.ca')) {
-	define('ROOT', 'http://ornyx.ca');
-} elseif (strstr($_SERVER['HTTP_HOST'], 'dev.ornyx.ca')) {
-	define('ROOT', 'http://dev.ornyx.ca');
-} else {
-	define('ROOT', 'https://ornyx.test');
-}
-
 # Split the path by '/'
 $params = explode('/', $_SERVER['REQUEST_URI']);
 $params = array_reverse($params);
 
 # Keeps users from requesting any file they want
-$safe_pages = array('accueil', 'personnages', 'geographie');
-$safe_tomes = array('tome-1', 'tome-2', 'tome-3');
+$safe_pages = [ 'accueil', 'personnages', 'geographie' ];
+$safe_tomes = [ 'tome-1', 'tome-2', 'tome-3' ];
 
-$sections = array(
-	'accueil' => array(
+$sections = [
+	'accueil' => [
 		'url'   => 'accueil',
-		'title' => ""
-	),
-	'romans' => array(
+		'title' => '',
+	],
+	'romans' => [
 		'url'   => 'romans',
-		'title' => 'Les Romans'
-	),
-	'personnages' => array(
+		'title' => 'Les Romans',
+	],
+	'personnages' => [
 		'url'   => 'personnages',
-		'title' => 'Les Personnages'
-	),
-	'geographie' => array(
+		'title' => 'Les Personnages',
+	],
+	'geographie' => [
 		'url'   => 'geographie',
-		'title' => 'La Geographie'
-	)
-);
+		'title' => 'La Geographie',
+	],
+];
 
 $in_first = in_array($params[0], $safe_pages);
 $in_second = in_array($params[1], $safe_pages);
 if ( $in_first || $in_second ) {
-	$mod = ($in_first ? array_search($params[0], $safe_pages) : array_search($params[1], $safe_pages) );
+	$mod = (
+		$in_first
+		? array_search($params[0], $safe_pages)
+		: array_search($params[1], $safe_pages)
+	);
 	$section = $safe_pages[$mod];
 } else {
 	$section = 'accueil';
@@ -47,7 +43,11 @@ if ( $in_first || $in_second ) {
 $in_first = in_array($params[0], $safe_tomes);
 $in_second = in_array($params[1], $safe_tomes);
 if ( $in_first || $in_second ) {
-	$mod = ($in_first ? array_search($params[0], $safe_tomes) : array_search($params[1], $safe_tomes) );
+	$mod = (
+		$in_first
+		? array_search($params[0], $safe_tomes)
+		: array_search($params[1], $safe_tomes)
+	);
 	$tome = $safe_tomes[$mod];
 } else {
 	$tome = '';
